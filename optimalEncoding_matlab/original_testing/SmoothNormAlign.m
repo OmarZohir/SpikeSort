532432320a390607369c7a9319016a8886880a1e
OmarZohir <misr1@hotmail.com>
2022-02-14 18:50:15 +0100

    Original Dataset

---
+++ optimalEncoding_matlab/original_testing/SmoothNormAlign.m
@@ -0,0 +1,46 @@
+function [RecAligned, InputAligned, RMSE] = SmoothNormAlign(reconstruction, Input, Nx, Time)
+    %Moving average of reconstructed signal
+    %sliding window of 10 signals
+    RecSmoothened(1,:) = movmean(reconstruction(1,:), 10);
+    RecSmoothened(2,:) = movmean(reconstruction(2,:), 10);
+%     RecSmoothened = reconstruction;
+ 
+    %Normalizing both signals
+    for i = 1:Nx
+        RecNormalized(i,:) = RecSmoothened(i,:)/max(RecSmoothened(i,:));
+        InputLNorm(i,:) = Input(i,:)/max(Input(i,:));
+    end
+
+    %Aligning the Normalized Input Signal and the reconstructed signal
+    %needs the signal processing toolbox
+    %Align each dimension on its own
+     %For Smoothened input, the delay on both dimensions shall be the same
+     %for i=1:size(InputLNorm,1)
+     %    [RecTemp(i,:), InputTemp(i,:),D] = alignsignals(RecNormalized(i,:), InputLNorm(i,:));
+     %end
+    
+    %For Non-smoothened input, delay only a single dimension, then apply
+    %the same delay to the second (other) dimension(s)
+     [RecTemp(1,:), InputTemp(1,:),D] = alignsignals(RecNormalized(1,:), InputLNorm(1,:));
+     for i= 2:size(InputLNorm,1)
+         InputTemp(i,:) = [zeros(1,abs(D)) InputLNorm(i,:)]; 
+         RecTemp(i,:) = RecNormalized(i,:);
+     end
+    
+    
+    %%The Input shall be delayed by D samples, discard the first 5*D samples in
+    %%both, and discard the last D samples in InputTemp
+    %InputAligned = zeros(Nx,Time - 5*abs(D));
+    %RecAligned = zeros(Nx,Time - 5*abs(D));
+    %an estimate of size of abs(D) being around 20 samples
+    InputAligned = zeros(Nx,Time - 100);
+    RecAligned = zeros(Nx,Time - 100);
+    startpoint = abs(100)+1;
+    endpoint = Time;
+    InputAligned = InputTemp(:,startpoint:endpoint);
+    RecAligned = RecTemp(:,startpoint:endpoint);
+    for i = 1:Nx
+        Error(i,:) = sqrt(mean((InputAligned(i,:) - RecAligned(i,:)).^2));
+    end
+    RMSE = sum(Error);
+end
